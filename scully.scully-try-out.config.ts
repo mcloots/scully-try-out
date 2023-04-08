4109b8e8663fc68f54c1e2be2b32720749c77bac
Michaël Cloots <michael.cloots@thomasmore.be>
2023-04-08 15:10:54 +0200

    adding scully to angular

---
+++ scully.scully-try-out.config.ts
@@ -0,0 +1,9 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'scully-try-out', //name of project
+  outDir: './dist/static', // directory for scully build artifacts
+  defaultPostRenderers: [],
+  routes: {},
+};
