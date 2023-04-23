bb6b03e7daa03d439a074fbf23203a805762e23f
Michaël Cloots <michael.cloots@thomasmore.be>
2023-04-23 15:57:35 +0200

    prism code snippet blogpost

--- blog/blogpost-2.md
+++ blog/blogpost-2.md
@@ -6,3 +6,15 @@ publishedAt: 2023-04-07T15:00:00.000+00:00
 ---
 
 # Blogpost 2
+
+```ts
+    // scully.your-app.config.file.ts
+    import 'prismjs/components/prism-javascript';
+    import 'prismjs/components/prism-typescript';
+    import 'prismjs/components/prism-markup';
+    import 'prismjs/components/prism-css';
+    import 'prismjs/components/prism-bash';
+    import 'prismjs/components/prism-json';
+    import 'prismjs/components/prism-markdown';
+    ...
+```
--- scully.scully-try-out.config.ts
+++ scully.scully-try-out.config.ts
@@ -1,4 +1,14 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-markdown';
+
+setPluginConfig('md', { enableSyntaxHighlighting: true });
 
 export const config: ScullyConfig = {
   projectRoot: './src',
